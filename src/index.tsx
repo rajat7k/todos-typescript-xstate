c441abb77879e2d928d7eaada42c98e1dc861b73
rajat7k <kaushikrajat777@gmail.com>
2023-02-28 18:06:03 +0530

    downgrade version

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import { Provider } from 'mobx-react'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import { todosStore } from './store'
 
 
-const root=ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement 
-)
+// const root=ReactDOM.createRoot(
+//     document.getElementById('root') as HTMLElement 
+// )
 
-root.render(
+// root.render(
+//     <React.StrictMode>
+//         <Provider todosStore={todosStore} >
+//         <App/>
+//         </Provider>
+//     </React.StrictMode>
+// )
+ReactDOM.render(
     <React.StrictMode>
-        <Provider todosStore={todosStore} >
-        <App/>
-        </Provider>
-    </React.StrictMode>
-)
+    <Provider todosStore={todosStore} >
+    <App/>
+    </Provider>
+</React.StrictMode>,
+    document.getElementById('root')
+  );
